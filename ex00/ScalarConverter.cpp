d389c50ac6461b23f3c184dd3c98dff9a528ddf5
ghostyghast <antoinethebestgamer@gmail.com>
2025-05-14 17:30:49 +0800

    feat: ex00 almost done

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -6,12 +6,13 @@
 /*   By: pringles <pringles@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 21:10:18 by pringles          #+#    #+#             */
-/*   Updated: 2025/04/30 21:56:27 by pringles         ###   ########.fr       */
+/*   Updated: 2025/05/02 04:14:50 by pringles         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 #include <cstdlib>
+#include <limits>
 #include <iostream>
 #include <climits>
 #include <cmath>
@@ -51,111 +52,151 @@ ScalarConverter    &ScalarConverter::operator=(const ScalarConverter &copy)
 	return(*this);
 }
 
-static int  getScalarType(string& literal)
+static int checkInput(const string& literal, int type)
 {
-	if (literal.size() == 1 && (isalpha(*literal.begin()) || ispunct(*literal.begin())))
-		return CHAR;
+	string::const_iterator	it = literal.begin();
+	string::const_iterator	end = literal.end();
+	
+	if (type == FLOAT && (literal.find('f') != literal.rfind('f')))
+		return -1;
+	if ((type == FLOAT || type == DOUBLE) && (literal.find('.') != literal.rfind('.')))
+		return -1;
+	if (strchr("+-", *it))
+		it++;
+	while (it != end)
+	{
+		if (type != INT && !strchr(".f", *it) && !isdigit(*it))
+			return -1;
+		if (type == INT && !isdigit(*it))
+			return -1;
+		it++;
+	}
+	return (type);
+}
+
+static int  getScalarType(const string& literal)
+{
+	int	type;
+
 	if (literal == "+inf" || literal == "-inf" || literal == "nan")
 		return DOUBLE;
 	if (literal == "+inff" || literal == "-inff" || literal == "nanf")
-		return FLOAT;	
+		return FLOAT;
+	if (literal.size() == 1 && (isalpha(*literal.begin()) || ispunct(*literal.begin())))
+		return CHAR;
+
 	if (literal.find('.') != string::npos)
 	{
 		if (literal[literal.size() - 1] == 'f')
-			return FLOAT;
-		return DOUBLE;
+			type = FLOAT;
+		else
+			type = DOUBLE;
 	}
-	return INT;
+	else
+		type = INT;
+	return (checkInput(literal, type));
 }
 
-static void convertFromInt(const string& literal)
+template <typename T> void toChar(T value)
 {
-	long	l = strtol(literal.c_str(), NULL , 10);
-	int		i = static_cast<int>(l);
-	double	d = static_cast<double>(i);
-	
 	cout << "char: ";
-	if (i > CHAR_MAX || i < CHAR_MIN)
+	if (value > std::numeric_limits<char>::max() || value < std::numeric_limits<char>::min() || std::isnan(value))
 		cout << "impossible";
-	else if (!std::isprint(i))
-		cout << "Non Displayable";
-	else
-		cout << '\'' << static_cast<char>(i) << '\'';
-	cout << '\n';
-
-	cout << "int: ";
-	if (l > INT_MAX || l < INT_MIN)
-		cout << "impossible";
-	else
-		cout << i;
-	cout << '\n';
-
-	cout << "float: ";
-	if (d > FLT_MAX || d < -FLT_MAX)
-		cout << "impossible";
-	else
-		cout << static_cast<float>(i);
-	cout << '\n';
-	cout << "double: ";
-	cout << d << '\n';	
-}
-
-static void	convertFromChar(char c)
-{
-	cout << "char: '" << c << "'\n";
-	cout << "int: " << static_cast<int>(c) << '\n';
-	cout << "float: " << static_cast<float>(c) << "f\n";
-	cout << "double: " << static_cast<double>(c) << '\n';
-}
-
-static void convertFromFloat(const string& literal)
-{
-	float	f = strtof(literal.c_str(), NULL);
-	
-	cout << "char: ";
-	if (f > CHAR_MAX || f < CHAR_MIN)
-		cout << "impossible";
-	else if (!isprint(static_cast<char>(f)))
+	else if (!isprint(static_cast<char>(value)))
 		cout << "non displayable";
 	else
-		cout << '\'' << static_cast<char>(f) << '\'';
+		cout << '\'' << static_cast<char>(value) << '\'';
 	cout << "\n";
-
-	cout << "int: ";
-	if (f > INT_MAX || f < INT_MIN || f == INFINITY || f == NAN)
-		cout << "impossible";
-	else
-		cout << static_cast<int>(f);
-	cout << "\n";
-	
-	cout << "float: ";
-	if (f == HUGE_VAL || (errno == ERANGE))
-		cout << "impossible";
-	else
-		cout << f << 'f';
-	cout << '\n';
-
-	cout << "double: " << static_cast<double>(f) << "\n";
 }
+
+template <typename T> void toInt(T value)
+{
+	cout << "int: ";
+	if ((value >= std::numeric_limits<int>::max() || value <= std::numeric_limits<int>::min() || std::isnan(value) || value == std::numeric_limits<T>::infinity()))
+		cout << "impossible";
+	else
+		cout << static_cast<int>(value);
+	cout << "\n";
+}
+
+template <typename T> void toFloat(T value)
+{
+	cout << "float: ";
+	if ((value != std::numeric_limits<float>::infinity() && value != -std::numeric_limits<float>::infinity()) &&
+		(value == std::numeric_limits<float>::max() || value > std::numeric_limits<float>::max() || value < -std::numeric_limits<float>::max()))
+		cout << "impossible";
+	else
+		cout << static_cast<float>(value) << 'f';
+	cout << '\n';
+}
+
+template <typename T> void toDouble(T value)
+{
+	cout << "double: ";
+	if ((value != std::numeric_limits<double>::infinity() && value != -std::numeric_limits<double>::infinity()) &&
+		(value == std::numeric_limits<double>::max() || value > std::numeric_limits<double>::max() || value < -std::numeric_limits<double>::max()))
+		cout << "impossible";
+	else
+		cout << static_cast<double>(value);	
+	cout << '\n';
+}
+
+template <typename T>	static void convertFromType(T value)
+{
+	toChar<T>(value);
+	toInt<T>(value);
+	toFloat<T>(value);
+	toDouble<T>(value);
+}
+
 
 void    ScalarConverter::convert(string literal)
 {
 	cout << std::fixed;
-	cout << std::setprecision(1);
+	// cout << std::setprecision(1);
 	switch (getScalarType(literal))
 	{
 		case CHAR:
-			convertFromChar(literal[0]);
+			convertFromType<char>(literal[0]);
 			break;
 		case INT:
-			convertFromInt(literal);
+			convertFromType<long>(strtol(literal.c_str(), NULL, 10));
 			break;
 		case FLOAT:
-			convertFromFloat(literal);
+			convertFromType<float>(strtof(literal.c_str(), NULL));
 			break;
 		case DOUBLE:
+			convertFromType<double>(strtod(literal.c_str(), NULL));
 			break;
 		default:
 			cout << "Invalid Input\n";
 	}
 }
+	// template <typename T>	void	covertFromType(T value)
+	// {
+	// 	cout << "char: ";
+	// 	if (value > std::numeric_limits<char>::max() || value < std::numeric_limits<char>::min())
+	// 		cout << "impossible";
+	// 	else if (!isprint(static_cast<char>(value)))
+	// 		cout << "non displayable";
+	// 	else
+	// 		cout << '\'' << static_cast<char>(value) << '\'';
+	// 	cout << "\n";
+	
+	// 	cout << "int: ";
+	// 	if (value > std::numeric_limits<int>::max() || value < std::numeric_limits<int>::max() || value == INFINITY || value ==inAN)
+	// 		cout << "impossible";
+	// 	else
+	// 		cout << static_cast<int>(value);
+	// 	cout << "\n";
+		
+	// 	cout << "float: ";
+	// 	if (value == std::numeric_limits<float>::max() || (errno == ERANGE))
+	// 		cout << "impossible";
+	// 	else
+	// 		cout << value << 'f';
+	// 	cout << '\n';
+	// 	if (value)
+	// 	cout << "double: " << static_cast<double>(value) << "\n";
+	// }
+	
